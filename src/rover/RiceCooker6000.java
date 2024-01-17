bd3d0498d06b455b8c259ba3ba0db8b0b88c5a1a
jennnniferkuang <94874342+jennnniferkuang@users.noreply.github.com>
2024-01-17 12:06:30 -0700

    Added comments and finalized code

--- src/rover/RiceCooker6000.java
+++ src/rover/RiceCooker6000.java
@@ -1,117 +1,140 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+/* Welcome to light detection rover, Rice Cooker 600! Here is what the rover does:
+ * 1. Button will switch between autonomous and manual control
+ * 2. Autonomous will:
+      - Detect if there is a bright light and move towards it
+      - Detect if there are obstacles in the way. If yes, turn and move in
+        opposite direction.
+      - If none of the above are detected, spin slowly and wait
+ * 3. Manual control will:
+     - Allow for thumbstick to control speed and direction of rover
  */
+
 package rover;
 
-//Add Phidgets Library
+// Add Phidgets Library
 import com.phidget22.*;
 
-/**
- *
- * @author 16088504
- */
 public class RiceCooker6000 {
 
-    /**
-     * @param args the command line arguments
-     */
+    // @param args the command line arguments
     public static void main(String[] args) throws Exception {
         
-        //Connect to wireless rover
+        // Connect to wireless rover
         Net.addServer("", "192.168.100.1", 5661, "", 0);
 
-        //Create
+        // Create motors
         DCMotor leftMotors = new DCMotor();
         DCMotor rightMotors = new DCMotor();
+        
+        // Create thumbstick axes
         VoltageRatioInput vAxis = new VoltageRatioInput(); 
         VoltageRatioInput hAxis = new VoltageRatioInput(); 
+        
+        // Create light sensor
         Light lightSensor = new Light();
+        
+        // Create sonar sensor
         DistanceSensor sonar = new DistanceSensor();
-        //Button greenButton = new Button();
+        
+        // Create button
+        Button butt = new Button(5, 673700);
 
-        //Address
+        // Set addresses
         leftMotors.setChannel(0);
         rightMotors.setChannel(1);
         vAxis.setChannel(0);
         hAxis.setChannel(1);
 
-        //Open
+        // Open
         leftMotors.open(5000);
         rightMotors.open(5000);
         vAxis.open(5000);
         hAxis.open(5000);
         sonar.open(5000);
 
-        //Increase acceleration 
+        // Increase acceleration (thumbstick)
         leftMotors.setAcceleration(leftMotors.getMaxAcceleration());
         rightMotors.setAcceleration(rightMotors.getMaxAcceleration());
 
+        // Initiate motor speed variables
         double leftMotorsSpeed = 0;
         double rightMotorsSpeed = 0;
         
+        // Initiate autonomous variable. Rover starts in manual control state
         boolean autonomous = false;
-        Button butt = new Button(5, 673700);
         
-        //Use your Phidgets
+        // 
         while(true){
+            // Check current button state and change autonomous variable accordingly
             autonomous = butt.state(autonomous);
-            //System.out.println(autonomous);
-            //System.out.println(lightSensor.getLight());
             
+            // Autonomous state
             if (autonomous){
+                
+                // Object detected
                 if (sonar.getDistance() < 400) {
-                        //Object detected! Stop motors
+                        // Stop motors
                         leftMotors.setTargetVelocity(0);
                         rightMotors.setTargetVelocity(0);
 
                         Thread.sleep(250);
-
+                        
+                        // Turn clockwise
                         leftMotors.setTargetVelocity(-0.5);
                         rightMotors.setTargetVelocity(0.5);
 
                         Thread.sleep(1500);
                         
+                        // Move forward
                         leftMotors.setTargetVelocity(-0.5);
                         rightMotors.setTargetVelocity(-0.5);
                         
                         Thread.sleep(1000);
-                }
+                } // end if object detected
+                
+                // Light detected
                 else if (lightSensor.getLight() > 50){
+                    // Move forward (towards light)
                     leftMotorsSpeed = -0.5;
                     rightMotorsSpeed = -0.5;
-                }
+                } // end else if light detected
+                
+                // No light or object detected
                 else{
+                    // Spin slowly clockwise
                     leftMotorsSpeed = -0.4;
                     rightMotorsSpeed = 0.4;
-                }
-            }
+                } // end else
+                
+            } // end Autonomous
             
+            // Manual control
             else{
 
-                //Get data from vertical and horizontal axis (values between -1 and 1)
+                // Get data from vertical and horizontal axis (values between -1 and 1)
                 double verticalAxis = vAxis.getVoltageRatio();
                 double horizontalAxis = hAxis.getVoltageRatio();
 
-                //Use thumbstick data to figure how each side of rover should move
+                // Use thumbstick data to figure how each side of rover should move
                 leftMotorsSpeed = verticalAxis + horizontalAxis;
                 rightMotorsSpeed = verticalAxis - horizontalAxis;
 
-                //Limit values to between -1 and 1
+                // Limit values to between -1 and 1
                 if(leftMotorsSpeed > 0.75) leftMotorsSpeed = 1;
                 if(leftMotorsSpeed < -0.75) leftMotorsSpeed = -1;
                 if(rightMotorsSpeed > 0.75) rightMotorsSpeed = 1;
                 if(rightMotorsSpeed < -0.75) rightMotorsSpeed = -1;
 
-
-
-                //Wait for 100 milliseconds
+                // Wait for 100 milliseconds
                 Thread.sleep(100);
             }
             
-            //Apply values 
+            // Apply values  from thumbstick to motors
             leftMotors.setTargetVelocity(leftMotorsSpeed);
             rightMotors.setTargetVelocity(rightMotorsSpeed);
-        }
-    }
-}
+            
+        } // End while
+        
+    } // End main method
+    
+} // End main class
